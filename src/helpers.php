a1ecba695240e1e82f13bf46a889fe0c5438b521
omarname <omarname@users.noreply.github.com>
2021-10-02 12:23:56 +0000

    Fix styling

--- src/helpers.php
+++ src/helpers.php
@@ -1,20 +1,20 @@
 <?php
 
-if (!function_exists('loyaltyReferLink')) {
+if (! function_exists('loyaltyReferLink')) {
     function loyaltyReferLink(string $code = ''): ?string
     {
         return config('yotpo.refer_link') . '/' . $code;
     }
 }
 
-if (!function_exists('loyaltyJsSdk')) {
+if (! function_exists('loyaltyJsSdk')) {
     function loyaltyJsSdk(): ?string
     {
         return '<script type="text/javascript" async src="' . config('yotpo.loyalty_js_sdk_url') . '"></script>';
     }
 }
 
-if (!function_exists('loyaltyJsModule')) {
+if (! function_exists('loyaltyJsModule')) {
     function loyaltyJsModule(): ?string
     {
         return '<script src="' . config('yotpo.loyalty_modules_loader_url') . '" async></script>';
